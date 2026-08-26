e82b3e8730836ab517a4a55b4ea4b984a5db9aa1
george-b-ai <balbackgeorge@gmail.com>
2026-08-26 18:18:21 -0400

    Initial commit

---
+++ about_me.py
@@ -0,0 +1,22 @@
+# My first Python program
+# This prints a personal introduction
+print("="*40)  # Prints a line of 40 equal signs
+print("Student Profile")
+print("="*40)  
+# Personal details stored as variables
+first_name = "Dzhordzh"
+last_name = "Balbakov"
+age = 46
+city = "New York"
+career = "ambulete driver" 
+# Display the introduction
+print("name:", first_name, last_name)
+print("age:", age)
+print("city:", city)
+print("Previous Career:", career)
+print("\nWhy I'm learning to code:")
+print("I want to learn how to code because I want to change my career and become a ai software developer.\n") 
+print("fun fact: I love raccoons\n")
+print("="*40)
+
+
